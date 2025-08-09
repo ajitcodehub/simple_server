b771988a679da927292d49cd016e27c8298e3bc3
ajitcodehub <nayakajitkumar743@gmail.com>
2025-08-09 06:07:38 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// server.js
+import ('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Sample route
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+// Sample POST route
+app.post('/greet', (req, res) => {
+  const { name } = req.body;
+  res.send(`Hello, ${name}!`);
+});
+
+app.get('/youtube', (req, res)=>{
+    res.send('chai and express');
+})
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
